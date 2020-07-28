32004c0324e2db8d1b4781a92823ae42c2c0c7cf
oussLakPro <oussama.laklalech.consulting@gmail.com>
2020-07-28 14:37:52 +0000

    [Stable] Add CSS style and more examples in the workspace

--- jsonforms-extension/src/browser/jsonforms-frontend-module.ts
+++ jsonforms-extension/src/browser/jsonforms-frontend-module.ts
@@ -6,6 +6,26 @@ import { JsonFormsOpenHandler } from "./jsonforms-open-handler";
 import { ReactTestWidget, ReactWidgetOptions } from "./react-test/react-test-widget";
 import { JsonFormsWidget, JsonschemaFormWidgetOptions } from "./jsonforms-widget";
 import { ReactTestOpenHandler } from "./react-test/react-test-open-handler";
+import { ThemeService } from "@theia/core/lib/browser/theming";
+
+const bootstrapLink = document.createElement("link");
+bootstrapLink.rel = 'stylesheet';
+document.head.appendChild(bootstrapLink);
+
+function updateTheme(newTheme: string): void {
+    const theme = bootstrapLink.getAttribute('theme');
+    if (theme === newTheme) {
+        return;
+    }
+    bootstrapLink.setAttribute('theme', newTheme);
+    bootstrapLink.href = newTheme === 'dark' ?
+        '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/darkly/bootstrap.min.css' :
+        '//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css';
+}
+updateTheme(ThemeService.get().getCurrentTheme().id);
+ThemeService.get().onThemeChange(({newTheme}) => updateTheme(newTheme.id));
+
+import '../../src/browser/style/index.css';
 
 export default new ContainerModule(bind => {
     // add your contribution bindings here
